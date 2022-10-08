7161d0dbec034ebaeedb6440f968087fe7393b93
Wojciech Turek <s21611@pjwstk.edu.pl>
2022-10-08 18:33:25 +0200

    file runs but, need to figure how to start fit our logic with easyAI

--- connect4.py
+++ connect4.py
@@ -1,17 +1,22 @@
 from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
 import numpy as np
 
-#Authors: Wojciech Turek, Paweł Badysiak
+
+# Authors: Wojciech Turek, Paweł Badysiak
 
 class Connect4(TwoPlayerGame):
 
-    def __init__(self, width = 7, height = 6):
+    def __init__(self, players):
         """Game Initialization"""
-        self.width = width
-        self.height = height
-        self.board = [['' for i in range(width)] for j in range(height) ]
+        self.players = players
+        self.width = 7
+        self.height = 6
+        self.pile = 20
+        self.current_player = 1
+        self.board = [['x' for i in range(self.width)] for j in range(self.height)]
 
-    def possible_moves(self): return ['0', '1', '2', '3', '4', '5', '6']
+    def possible_moves(self):
+        return ['0', '1', '2', '3', '4', '5', '6']
 
     def make_move(self, move):
         player_move = int(move)
@@ -21,14 +26,18 @@ class Connect4(TwoPlayerGame):
             if '' not in self.board[int(move)][i]:
                 print("todo")
 
+    def win(self):
+        return self.pile <= 0
 
-    def win(self): return self.pile <= 0
+    def is_over(self):
+        return self.win()
 
-    def is_over(self): return self.win()
+    def show(self):
+        print("%d bones left in the self pile" % self.pile)
 
-    def show(self): print("%d bones left in the self pile" % self.pile)
+    def scoring(self):
+        return 100 if self.win() else 0
 
-    def scoring(self): return 100 if self.win() else 0
 
 ai = Negamax(13)
 game = Connect4([Human_Player(), AI_Player(ai)])
