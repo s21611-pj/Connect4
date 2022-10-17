ccbfad64cabcd9f5b6a6e4dd87155d4a84881fd5
badekk <pawel.badysiak@gmail.com>
2022-10-17 20:26:00 +0200

    Changed some logic regarding making move and provide horizontal win

--- connect4.py
+++ connect4.py
@@ -1,44 +1,71 @@
 from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
 import numpy as np
 
-
 # Authors: Wojciech Turek, Paweł Badysiak
 
 class Connect4(TwoPlayerGame):
 
     def __init__(self, players):
-        """Game Initialization"""
+        """Game Initialization with fixed board size"""
         self.players = players
+        self.current_player = 1
         self.width = 7
         self.height = 6
-        self.pile = 20
-        self.current_player = 1
         self.board = [['x' for i in range(self.width)] for j in range(self.height)]
-        self.counter = 0  # human even, ai odd
+        self.roundCounter = 0  # human even, ai odd
+        self.HumanPlayerSymbol = 'PL'
+        self.AIPlayerSymbol = 'AI'
 
     def possible_moves(self):
         return ['0', '1', '2', '3', '4', '5', '6']
 
     def make_move(self, move):
+        """ TBD """
         player_move = int(move)
         for i in range(self.height - 1, -1, -1):
             if 'x' in self.board[i][player_move]:
-                self.board[i][int(move)] = '@'
+                if self.roundCounter % 2 == 0:
+                    self.board[i][int(move)] = self.HumanPlayerSymbol
+                else:
+                    self.board[i][int(move)] = self.AIPlayerSymbol
+                self.roundCounter += 1
                 return self.board
 
     def win(self):
-        return self.pile <= 0
+        """ TBD """
+        symbol = ''
+        if (self.roundCounter -1) % 2 == 0:
+            symbol = self.HumanPlayerSymbol
+        else:
+            symbol = self.AIPlayerSymbol
+
+        # Check for win horizontaly
+        for x in range(self.height):
+            counter = 0
+            for y in range(self.width):
+                if self.board[x][y] == symbol:
+                    counter += 1
+                else:
+                    counter = 0
+                if counter >= 4:
+                    return True
+        # Check for win verticaly
+        # Check for win diagonally
+
+        return False
 
     def is_over(self):
+        """ Game is over when someone wins """
         return self.win()
 
     def show(self):
+        """ Prints whole board after each move """
         print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in self.board]))
 
     def scoring(self):
-        return 100 if self.win() else 0
+        """ Assigns one point to winner """
+        return 1 if self.win() else 0
 
-
-ai = Negamax(13)
+ai = Negamax(1)
 game = Connect4([Human_Player(), AI_Player(ai)])
 history = game.play()
