58ed187e76f9a1337fd37ad8665369a4470bc263
Wojciech Turek <turekwojtek@gmail.com>
2022-10-19 20:32:56 +0200

    Final commit

--- connect4.py
+++ connect4.py
@@ -16,18 +16,26 @@ import numpy as np
 from scipy.signal import convolve2d
 
 
-def get_detection_kernels():
-    horizontal_kernel = np.array([[1, 1, 1, 1]])
-    vertical_kernel = np.transpose(horizontal_kernel)
-    diag1_kernel = np.eye(4, dtype=np.uint8)
-    diag2_kernel = np.fliplr(diag1_kernel)
-    detection_kernels = [horizontal_kernel, vertical_kernel, diag1_kernel, diag2_kernel]
-    return detection_kernels
-
 class Connect4(TwoPlayerGame):
+
+    @staticmethod
+    def get_detection_kernels():
+        """
+            Create kernels for horizontal, vertical and diagonal win detection.
+            Create a 2D array from game board
+            Returns:
+                detection_kernels (list): List of 4 possible kernels to detect
+        """
+        horizontal_kernel = np.array([[1, 1, 1, 1]])
+        vertical_kernel = np.transpose(horizontal_kernel)
+        diagonal1_kernel = np.eye(4, dtype=np.uint8)
+        diagonal2_kernel = np.fliplr(diagonal1_kernel)
+        detection_kernels = [horizontal_kernel, vertical_kernel, diagonal1_kernel, diagonal2_kernel]
+        return detection_kernels
 
     def __init__(self, players):
         """Game Initialization with fixed board size filled with zeroes
+            Current Player fixed to 1 (Human), if you want AI to start, just change this value to "2"
 
         Parameters:
             players (list): List containing 2 players
@@ -37,31 +45,38 @@ class Connect4(TwoPlayerGame):
         self.width = 7
         self.height = 6
         self.board = np.array([[0 for x in range(self.width)] for y in range(self.height)])
-        self.roundCounter = 0
 
     def possible_moves(self):
         """ Returns:
-            list: List of possible moves.
+                list: List of possible moves.
         """
         return [i for i in range(self.width) if (self.board[:, i].min() == 0)]
 
     def make_move(self, move):
         """ Places a piece on the bottom of the board.
             Parameters:
-            move (int): Position on the board.
+                move (int): Position on the board.
         """
         line = np.argmin(self.board[:, move] != 0)
         self.board[line, move] = self.current_player
 
     def lose(self):
-        for kernel in get_detection_kernels():
+        """
+            Run the board through the convolution operations
+            using Scipy's highly optimized convolve2d function.
+            In the array formed by the convolution output, any "4"
+            indicates that there were 4 connected tiles in the board.
+            Returns:
+                 bool: True if found 4 connected tiles, default False
+        """
+        for kernel in self.get_detection_kernels():
             if (convolve2d(self.board == self.opponent_index, kernel, mode="valid") == 4).any():
                 return True
         return False
 
     def is_over(self):
         """ Returns:
-            bool: True if the game is over.
+                bool: True if the game is over.
         """
         return self.lose()
 
@@ -71,12 +86,15 @@ class Connect4(TwoPlayerGame):
         print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in reverse_board]))
 
     def scoring(self):
-        """ Decreases one point from the loser """
+        """
+            Decreases one point from the loser
+            Returns:
+                -1 point if current_player loses game, default 0
+        """
         return -1 if self.lose() else 0
 
 
 if __name__ == "__main__":
-
     """
         Game runner
         Setting up Negamax to 7 moves ahead intelligent
